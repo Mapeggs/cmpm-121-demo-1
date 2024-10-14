d38bd341cb2bb4648c4a9123b365cab9a94bafbd
Mapeggs <anguy410@ucsc.edu>
2024-10-13 17:35:27 -0700

    completed step 1

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,16 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+//create a button element
+const button: HTMLButtonElement = document.createElement("button");
+
+//create a eventlistener for button
+button.addEventListener('click', () => {
+    console.log('Button Clicked');
+});
+
+//append button to the dom
+document.body.appendChild(button);
+
+
